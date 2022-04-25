ac9018526473102d0fc1857e5d3b273bea2e6601
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-04-25 13:26:22 +0300

    Change deprecated methods. Add main function

--- 2048-hack.py
+++ 2048-hack.py
@@ -4,29 +4,60 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import logging
 import time
 import random
 
-logging.basicConfig(
-    filename='logs.txt', level=logging.DEBUG, 
-    format='%(asctime)s - %(levelname)s - %(message)s'
-    )
 
-browser = webdriver.Firefox()
-browser.get('https://play2048.co')
+def get_field() -> list:
+    # class='tile-container'
+    pass
 
-htmlElem = browser.find_element_by_tag_name('html')
-# TODO: while not game over cycle
-# random directions test
-for _ in range(100):
-    direction = random.randint(0, 3)
-    if direction == 0:
-        htmlElem.send_keys(Keys.UP)
-    elif direction == 1:
-        htmlElem.send_keys(Keys.DOWN)
-    elif direction == 2:
-        htmlElem.send_keys(Keys.LEFT)
-    else:
-        htmlElem.send_keys(Keys.RIGHT)
-    time.sleep(0.4)
+
+def next_move() -> str:
+    pass
+
+
+def main():
+    logging.basicConfig(
+        filename='logs.txt', level=logging.DEBUG, 
+        format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+    browser = webdriver.Firefox()
+    browser.get('https://play2048.co')
+    browser.maximize_window()
+
+    htmlElem = browser.find_element(By.TAG_NAME, 'html')
+    status = None
+    # TODO: while not game over cycle
+    # random directions test
+    for _ in range(100):
+        logging.info(f'{status} - iteration {_}')
+        direction = random.randint(0, 3)
+        if direction == 0:
+            htmlElem.send_keys(Keys.UP)
+        elif direction == 1:
+            htmlElem.send_keys(Keys.DOWN)
+        elif direction == 2:
+            htmlElem.send_keys(Keys.LEFT)
+        else:
+            htmlElem.send_keys(Keys.RIGHT)
+        time.sleep(0.4)
+    
+        # find element with class name game-message
+        try:
+            statusElem = browser.find_element(By.CLASS_NAME, 'game-message')
+            status = statusElem.text
+        except:
+            pass
+        
+    # get total score in the end of the game
+    score = 0
+    print(f'Game over. Result - {score}')
+    browser.close()
+
+
+if __name__=='__main__':
+    main()
